22632321de3575cb853e7f04e7170884383118a9
Kevin Harrington <mad.hephaestus@gmail.com>
2023-07-25 10:20:08 -0400

    all three links

--- MarcosCad.groovy
+++ MarcosCad.groovy
@@ -1421,6 +1421,7 @@ DHParameterKinematics limb = gen.getByName(mb,"RightFront")
 return [
 	gen.generateCad(limb,0)
 	,gen.generateCad(limb,1)
+	,gen.generateCad(limb,2)
 	//,gen.generateBody(mb)
 ]
 return gen
