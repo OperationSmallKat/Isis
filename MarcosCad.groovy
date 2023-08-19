40cd9a819be6a22fbd80b31fb86a4d8093a5200c
Kevin Harrington <mad.hephaestus@gmail.com>
2023-08-19 12:17:56 -0400

    auto-save in ScriptingEngine.resolveConflict

--- MarcosCad.groovy
+++ MarcosCad.groovy
@@ -1553,7 +1553,7 @@ MobileBase mb = (MobileBase)DeviceManager.getSpecificDevice("Marcos");
 gen.setMobileBase(mb)
 DHParameterKinematics limb = gen.getByName(mb,"RightFront")
 return [
-	//gen.generateCad(limb,0)
+	gen.generateCad(limb,0),
 	//,gen.generateCad(limb,1),
 	//gen.generateCad(limb,2)
 	//,gen.generateBody(mb)
